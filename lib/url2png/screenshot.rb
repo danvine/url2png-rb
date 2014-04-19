0db13f5c34761b508977476711dee1ae3fd085d8
danvine <github@danvine.com>
2014-04-19 14:40:02 -0400

    variablize the domain

--- lib/url2png/screenshot.rb
+++ lib/url2png/screenshot.rb
@@ -1,14 +1,17 @@
 module Url2png
   class Screenshot
-    attr_reader :apikey, :secret, :query_string, :token, :options
+    attr_reader :apikey, :secret, :query_string, :token, :options, :domain
 
     def initialize options
 
       @apikey = options[:apikey] || ENV['URL2PNG_APIKEY']
       @secret = options[:secret] || ENV['URL2PNG_SECRET']
+      @domain = options[:domain] || 'api.url2png.com'
 
-      options.delete(:apikey) # we don't want these in the query string
-      options.delete(:secret) # we don't want these in the query string
+      options.delete(:apikey) # we don't want this in the query string
+      options.delete(:secret) # we don't want this in the query string
+      options.delete(:domain) # we don't want this in the query string
+
       @options = options
 
     end
@@ -22,7 +25,7 @@ module Url2png
     end
 
     def url
-      "http://api.url2png.com/v6/#{self.apikey}/#{self.token}/png/?#{self.query_string}"
+      "http://#{self.domain}/v6/#{self.apikey}/#{self.token}/png/?#{self.query_string}"
     end
 
     def to_s
